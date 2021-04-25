6c1124db0f3c4f2d6d9394c2c94386bce95e71c1
Antonio José Martínez <7965897@alu.murciaeduca.es>
2021-04-25 23:38:06 +0200

    primer commit

---
+++ hola-mundo/src/hola/mundo/HolaMundo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola.mundo;
+
+/**
+ *
+ * @author ToniM
+ */
+public class HolaMundo {
+
+    /**
+     * @param args esto es una prueba
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
